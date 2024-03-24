fcc4e20c0c4b616f57e886479faaf7340e2e878f
Blake <epicbwo@gmail.com>
2024-03-24 19:33:01 -0400

    Readded firebase configurations

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQrtfgyodGtwzJ47Af8rit8VIi_SEkEK0",
+  authDomain: "ufl-recycle-app.firebaseapp.com",
+  projectId: "ufl-recycle-app",
+  storageBucket: "ufl-recycle-app.appspot.com",
+  messagingSenderId: "68192476406",
+  appId: "1:68192476406:web:36ef4f9f28ef6591d260d7"
+};
+
+const app = initializeApp(firebaseConfig);
