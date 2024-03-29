3b0875e4dac3bfca7d0b8ad059ac9bf705c3781b
Griffin C Balducci <93067242+Griffinshark@users.noreply.github.com>
2024-03-28 23:21:16 -0400

    Bugfix and style change of nav buttons
    - Buttons now change color and cursor on hover
    - Button size and font size changed

--- content.js
+++ content.js
@@ -9,15 +9,25 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function getNav() {
-    document.getElementById("buttonNavHome").onclick = function () {
-        fetch('home.html')
+    // Homepage Button:
+    document.getElementById('buttonNavHome').onclick = function () {
+        fetch('href=home.html')
             .then(response => response.text())
             .then(html => {
                 document.getElementById('app-content').innerHTML = html;
             })
     };
 
-    document.getElementById("buttonNavPointBank").onclick = function () {
+    document.getElementById('buttonNavHome').onmouseover = function () {
+        document.getElementById('buttonNavHome').style.backgroundColor = '#848482';
+    };
+
+    document.getElementById('buttonNavHome').onmouseout = function () {
+        document.getElementById('buttonNavHome').style.backgroundColor = '#A8A9AD';
+    };
+
+    // Point Bank Button:
+    document.getElementById('buttonNavPointBank').onclick = function () {
         fetch('point_bank.html')
             .then(response => response.text())
             .then(html => {
@@ -25,7 +35,16 @@ function getNav() {
             })
     };
 
-    document.getElementById("buttonNavShop").onclick = function () {
+    document.getElementById('buttonNavPointBank').onmouseover = function () {
+        document.getElementById('buttonNavPointBank').style.backgroundColor = '#848482';
+    };
+
+    document.getElementById('buttonNavPointBank').onmouseout = function () {
+        document.getElementById('buttonNavPointBank').style.backgroundColor = '#A8A9AD';
+    };
+
+    // Shop Button:
+    document.getElementById('buttonNavShop').onclick = function () {
         fetch('shop.html')
             .then(response => response.text())
             .then(html => {
@@ -33,7 +52,16 @@ function getNav() {
             })
     };
 
-    document.getElementById("buttonNavInfo").onclick = function () {
+    document.getElementById('buttonNavShop').onmouseover = function () {
+        document.getElementById('buttonNavShop').style.backgroundColor = '#848482';
+    };
+
+    document.getElementById('buttonNavShop').onmouseout = function () {
+        document.getElementById('buttonNavShop').style.backgroundColor = '#A8A9AD';
+    };
+
+    // Info Button:
+    document.getElementById('buttonNavInfo').onclick = function () {
         fetch('rec_info.html')
             .then(response => response.text())
             .then(html => {
@@ -41,7 +69,16 @@ function getNav() {
             })
     };
 
-    document.getElementById("buttonNavLeaderboard").onclick = function () {
+    document.getElementById('buttonNavInfo').onmouseover = function () {
+        document.getElementById('buttonNavInfo').style.backgroundColor = '#848482';
+    };
+
+    document.getElementById('buttonNavInfo').onmouseout = function () {
+        document.getElementById('buttonNavInfo').style.backgroundColor = '#A8A9AD';
+    };
+
+    // Leaderboard Button:
+    document.getElementById('buttonNavLeaderboard').onclick = function () {
         fetch('leaderboard.html')
             .then(response => response.text())
             .then(html => {
@@ -49,7 +86,16 @@ function getNav() {
             })
     };
 
-    document.getElementById("buttonNavProfile").onclick = function () {
+    document.getElementById('buttonNavLeaderboard').onmouseover = function () {
+        document.getElementById('buttonNavLeaderboard').style.backgroundColor = '#848482';
+    };
+
+    document.getElementById('buttonNavLeaderboard').onmouseout = function () {
+        document.getElementById('buttonNavLeaderboard').style.backgroundColor = '#A8A9AD';
+    };
+
+    // Profile Button:
+    document.getElementById('buttonNavProfile').onclick = function () {
         fetch('profile.html')
             .then(response => response.text())
             .then(html => {
@@ -57,7 +103,16 @@ function getNav() {
             })
     };
 
-    document.getElementById("buttonNavLogin").onclick = function () {
+    document.getElementById('buttonNavProfile').onmouseover = function () {
+        document.getElementById('buttonNavProfile').style.backgroundColor = '#848482';
+    };
+
+    document.getElementById('buttonNavProfile').onmouseout = function () {
+        document.getElementById('buttonNavProfile').style.backgroundColor = '#A8A9AD';
+    };
+
+    // Login Button:
+    document.getElementById('buttonNavLogin').onclick = function () {
         fetch('login.html')
             .then(response => response.text())
             .then(html => {
@@ -67,7 +122,16 @@ function getNav() {
             })
     };
 
-    document.getElementById("buttonNavRegister").onclick = function () {
+    document.getElementById('buttonNavLogin').onmouseover = function () {
+        document.getElementById('buttonNavLogin').style.backgroundColor = '#848482';
+    };
+
+    document.getElementById('buttonNavLogin').onmouseout = function () {
+        document.getElementById('buttonNavLogin').style.backgroundColor = '#A8A9AD';
+    };
+
+    // Register Button:
+    document.getElementById('buttonNavRegister').onclick = function () {
         fetch('register.html')
             .then(response => response.text())
             .then(html => {
@@ -75,7 +139,7 @@ function getNav() {
 
                 getNav();
 
-                document.getElementById("buttonNavLogin2").onclick = function () {
+                document.getElementById('buttonNavLogin2').onclick = function () {
                     fetch('login.html')
                         .then(response => response.text())
                         .then(html => {
@@ -85,5 +149,13 @@ function getNav() {
                         })
                 };
             })
+    };
+
+    document.getElementById('buttonNavRegister').onmouseover = function () {
+        document.getElementById('buttonNavRegister').style.backgroundColor = '#848482';
+    };
+
+    document.getElementById('buttonNavRegister').onmouseout = function () {
+        document.getElementById('buttonNavRegister').style.backgroundColor = '#A8A9AD';
     };
 }
